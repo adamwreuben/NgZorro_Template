22e94f0f56aad707ccfff36ecafd1ae8c3b1a0be
Adam Reuben <adamreuben66@gmail.com>
2022-08-18 18:44:19 +0300

    NGZORRO START CODES

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,6 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: "AIzaSyCAdTNcWRj9cJsZ6XR-EZS8uKDpAFKRN84",
-    authDomain: "fir-lipa.firebaseapp.com",
-    databaseURL: "https://fir-lipa-default-rtdb.firebaseio.com",
-    projectId: "fir-lipa",
-    storageBucket: "fir-lipa.appspot.com",
-    messagingSenderId: "337046535881",
-    appId: "1:337046535881:web:12c382bee4c2e96b225718",
-    measurementId: "G-Q6JZDTTRVG"
+    
   }
 };
